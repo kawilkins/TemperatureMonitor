56658f9c132c00fa0b15314970aa3d42616bde6d
kwilkins <kwilkins@mail.com>
2022-02-05 12:31:12 -0600

    Added new file tempmonCPU.cpp.

---
+++ src/tempmonCPU.cpp
@@ -0,0 +1,25 @@
+/*==============================================================
+Program:  Mathematica
+Version:  0.1.0
+Version Date:  02/05/2022
+Author:  Kevin Wilkins
+Date:  02/05/2022
+Parameters:
+This application will read out CPU temperature data from 
+'/sys/class/thermal/thermal_zone0/temp' file on Linux and will
+convert output to human readable format.
+==============================================================*/
+
+/*################################################
+Preprocessor directives
+################################################*/
+#include <iostream>
+#include <fstream>
+
+/*################################################
+Main function
+################################################*/
+int main()
+{
+    return 0;
+}
