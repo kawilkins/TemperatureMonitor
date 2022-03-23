65351fe374e94addd3ac96659489894183fd2b7b
kwilkins <kwilkins@mail.com>
2022-03-23 12:39:35 -0500

    Created function for fetching information from /sys/class/thermal/thermal_zone0/temp.
    This will write to variable and then pass variable back to main().

--- src/tempmonCPU.cpp
+++ src/tempmonCPU.cpp
@@ -10,16 +10,51 @@ This application will read out CPU temperature data from
 convert output to human readable format.
 ==============================================================*/
 
-/*################################################
-Preprocessor directives
-################################################*/
+// Preprocessor directives and namespace usage
 #include <iostream>
 #include <fstream>
 
-/*################################################
-Main function
-################################################*/
+using namespace std;
+
+// Function Prototypes
+int Thermal();
+
+// Main function
 int main()
 {
+    int sysTemp = 0;
+
+    // Use Thermal function to get system thermal info
+    Thermal();
+
+    // Output to human readable format
+    cout << "System temperature:  " << sysTemp << "\n\n";
+    
+    // Exit program without errors
     return 0;
+}
+
+// Get thermal information from system
+int Thermal()
+{
+    int sysTemp = 0;
+    ifstream sysThermal ("/sys/class/thermal/thermal_zone0/temp");
+
+    // Check if file is open and send information to variable
+    if (systemThermal.is_open())
+    {
+        // Write output of file to sysTemp variable
+        while (getline (systemThermal,sysTemp))
+        {
+            cin >> sysTemp;
+        }
+        sysThermal.close();
+    }
+    else
+    {
+        cout << "Failure to open '/sys/class/thermal/thermal_zone0/temp'\n"
+             << "Please try again . . .\n\n";
+    }
+
+    return sysTemp;
 }
