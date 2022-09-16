d8f53a9a379e58222212f3ab2527e2b7f7613a75
kwilkins <kwilkins@mail.com>
2022-09-15 21:47:21 -0500

    Remove excessive and unnecessary comments. Add definition for double line spacing. Add definition for color formatting. Remove unnecessary variable that converted string to double. Simplified Celsius conversion. Formatted spacing and alignment of output. Add evaluation of temperature data and corresponding text color. Add status output. Add message to user.

--- src/tempmonCPU.cpp
+++ src/tempmonCPU.cpp
@@ -1,7 +1,7 @@
 /*==============================================================
 Program:  CPU Temperature Monitor
 Version:  0.2.1
-Version Date:  09/15/2022
+Version Date:  09/06/2022
 Author:  Kevin Wilkins
 Date:  02/05/2022
 Parameters:
@@ -19,15 +19,21 @@ degrees Fahrenheit, and Kelvin.
 
 using namespace std;
 
+// Special definitions
+#define DOUBLELINE "\n\n"
+#define RESTORE "\033[0m" // Return to default terminal color
+#define GREEN "\033[1m\033[32m" // Bold green
+#define YELLOW "\033[1m\033[33m" // Bold yellow
+#define RED "\033[1m\033[31m" // Bold red
+
 // Main function
 int main()
 {
     ifstream thermal;
-    string sysTemp = ""; // Will store the output of thermal
+    string sysTemp = "";
     
     // Read in /sys/.../temp file
-    // Point read pointer to beginning of file
-    thermal.open ("/sys/class/thermal/thermal_zone0/temp", ios::in);
+    thermal.open("/sys/class/thermal/thermal_zone0/temp", ios::in);
 
     // Check if file is open and output in human readable format
     if (thermal.is_open())
@@ -35,24 +41,52 @@ int main()
         while (getline (thermal, sysTemp))
         {
             // Convert output
-            double tempLong = stod(sysTemp); // Convert string to double
-            double tempCelsius = tempLong / 1000; // Convert output to human readable
-            double tempFahrenheit = (tempCelsius * 1.8) + 32; // Convert to Fahrenheit
-            double tempKelvin = tempCelsius + 273.15; // Convert to Kelvin
+            double tempCelsius = stod(sysTemp) / 1000;
+            double tempFahrenheit = (tempCelsius * 1.8) + 32;
+            double tempKelvin = tempCelsius + 273.15;
+            string colorCode = "";
+            string status = "";
+            string message = "";
             
-            // Output to human readable format
-            cout << fixed << setprecision(3); // Use 3 significant digits
-            cout << "System temperature:  \n"
-                 << setw(10) << right << tempCelsius << " \u00B0C\n"
+            // Evaluate Temperature Safety Level
+            if(tempCelsius < 60)
+            {
+                colorCode = GREEN;
+                status = "GOOD";
+                message = "CPU temperature is very good.";
+            }
+            else if(tempCelsius >= 60 && tempCelsius < 75)
+            {
+                colorCode = YELLOW;
+                status = "CAUTION";
+                message = "CPU temperature is somewhat elevated.";
+            }
+            else if(tempCelsius >= 75)
+            {
+                colorCode = RED;
+                status = "WARNING";
+                message = "CPU temperature is high and needs attention.";
+            }
+            
+            // Output to terminal as human readable
+            cout << fixed << setprecision(3);
+            cout << "System Temperature\n";
+            cout << "Status:" << colorCode << status << RESTORE;
+            cout << DOUBLELINE;
+            cout << colorCode;
+            cout << setw(10) << right << tempCelsius << " \u00B0C\n"
                  << setw(10) << right << tempFahrenheit << " \u00B0F\n"
-                 << setw(10) << right << tempKelvin << "  K\n";
+                 << setw(10) << right << tempKelvin << "  K" << DOUBLELINE;
+            cout << RESTORE;
+            cout << message << DOUBLELINE;
         }
         thermal.close();
     }
-    else // File does not open
+    else
     {
         cout << "Failure to open '/sys/class/thermal/thermal_zone0/temp'\n"
-             << "Please try again . . .\n\n";
+             << "Please try again . . ."
+             << DOUBLELINE;
     }
 
     // Exit program without errors
