09f168d5567e7f0e40d84b8c0f1fb9b090c00194
kwilkins <kwilkins@mail.com>
2023-01-08 16:35:35 -0600

    Updated syntax to standard. Remove 'using namespace std;'. Remove unnecessary white space around '{}'.

--- src/tempmon.cpp
+++ src/tempmon.cpp
@@ -1,11 +1,11 @@
 /*==============================================================
 Program:  CPU Temperature Monitor
-Version:  0.2.1
+Version:  0.2.2
 Version Date:  09/06/2022
 Author:  Kevin Wilkins
 Date:  02/05/2022
 Parameters:
-This application will read out CPU temperature data from 
+This application will read out CPU temperature data from
 '/sys/class/thermal/thermal_zone0/temp' file on Linux and will
 convert output to human readable format in degrees Celsius, 
 degrees Fahrenheit, and Kelvin.
@@ -17,8 +17,6 @@ degrees Fahrenheit, and Kelvin.
 #include <fstream>
 #include <string>
 
-using namespace std;
-
 // Special definitions
 #define DOUBLELINE "\n\n"
 #define RESTORE "\033[0m" // Return to default terminal color
@@ -27,66 +25,54 @@ using namespace std;
 #define RED "\033[1m\033[31m" // Bold red
 
 // Main function
-int main()
-{
-    ifstream thermal;
-    string sysTemp = "";
+int main(){
+    std::ifstream thermal;
+    std::string sysTemp = "";
     
     // Read in /sys/.../temp file
-    thermal.open("/sys/class/thermal/thermal_zone0/temp", ios::in);
+    thermal.open("/sys/class/thermal/thermal_zone0/temp", std::fstream::in);
 
     // Check if file is open and output in human readable format
-    if (thermal.is_open())
-    {
-        while (getline (thermal, sysTemp))
-        {
+    if (thermal.is_open()){
+        while (getline (thermal, sysTemp)){
             // Convert output
             double tempCelsius = stod(sysTemp) / 1000;
             double tempFahrenheit = (tempCelsius * 1.8) + 32;
             double tempKelvin = tempCelsius + 273.15;
-            string colorCode = "";
-            string status = "";
-            string message = "";
+            std::string colorCode = "";
+            std::string status = "";
+            std::string message = "";
             
             // Evaluate Temperature Safety Level
-            if(tempCelsius < 60)
-            {
+            if(tempCelsius < 60){
                 colorCode = GREEN;
                 status = "GOOD";
                 message = "CPU temperature is very good.";
-            }
-            else if(tempCelsius >= 60 && tempCelsius < 75)
-            {
+            }else if(tempCelsius >= 60 && tempCelsius < 75){
                 colorCode = YELLOW;
                 status = "CAUTION";
                 message = "CPU temperature is somewhat elevated.";
-            }
-            else if(tempCelsius >= 75)
-            {
+            }else if(tempCelsius >= 75){
                 colorCode = RED;
                 status = "WARNING";
                 message = "CPU temperature is high and needs attention.";
             }
             
             // Output to terminal as human readable
-            cout << fixed << setprecision(3);
-            cout << "System Temperature\n";
-            cout << "Status:" << colorCode << status << RESTORE;
-            cout << DOUBLELINE;
-            cout << colorCode;
-            cout << setw(10) << right << tempCelsius << " \u00B0C\n"
-                 << setw(10) << right << tempFahrenheit << " \u00B0F\n"
-                 << setw(10) << right << tempKelvin << "  K" << DOUBLELINE;
-            cout << RESTORE;
-            cout << message << DOUBLELINE;
+            std::cout << std::fixed << std::setprecision(3);
+            std::cout << "System Temperature\n";
+            std::cout << "Status:" << colorCode << status << RESTORE;
+            std::cout << DOUBLELINE;
+            std::cout << colorCode;
+            std::cout << std::setw(10) << std::right << tempCelsius << " \u00B0C\n";
+            std::cout << std::setw(10) << std::right << tempFahrenheit << " \u00B0F\n";
+            std::cout << std::setw(10) << std::right << tempKelvin << "  K";
+            std::cout << DOUBLELINE << RESTORE << message << DOUBLELINE;
         }
         thermal.close();
-    }
-    else
-    {
-        cout << "Failure to open '/sys/class/thermal/thermal_zone0/temp'\n"
-             << "Please try again . . ."
-             << DOUBLELINE;
+    }else{
+        std::cout << "Failure to open '/sys/class/thermal/thermal_zone0/temp'\n";
+        std::cout << "Please try again . . ." << DOUBLELINE;
     }
 
     // Exit program without errors
